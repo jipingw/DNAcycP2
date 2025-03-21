d0bc83876d5cd576f8a622064e3bd327b610fa6c
Dr. Ji-Ping Wang <jzwang@northwestern.edu>
2025-03-21 15:51:52 -0500

    basilisk.R

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,12 +1,13 @@
-#' @importFrom basilisk BasiliskEnvironment
+k#' @importFrom basilisk BasiliskEnvironment
 env1 <- BasiliskEnvironment("env1", pkgname="DNAcycP2",
                             packages=c(
                                 'python==3.11',
                                 'pandas==2.1.2',
+                                'tensorflow==2.14.0',
+                                'keras==2.14.0',
                                 'docopt==0.6.2'),
                             pip=c(
                                 'numpy==1.26.1',
-                                'bio==1.7.1',
-                                'tensorflow==2.14.0',
-                                'keras==2.14.0'),
+                                'bio==1.7.1'),
                             path="dnacycp_python")
+
